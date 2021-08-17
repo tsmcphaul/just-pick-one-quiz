4705884e3cff51e3df95fa9da294a00ef4669d8b
Tammarra McPhaul <shaemc22@gmail.com>
2021-08-17 19:57:38 -0400

    first commit

---
+++ backend/db/migrate/20210817233301_create_answers.rb
@@ -0,0 +1,10 @@
+class CreateAnswers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :answers do |t|
+      t.string :content
+      t.boolean :correct_answer
+
+      t.timestamps
+    end
+  end
+end
