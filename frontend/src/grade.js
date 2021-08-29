27446fe7625d7bd66ad04d35c75ca9069b2908dd
Tammarra McPhaul <shaemc22@gmail.com>
2021-08-29 13:33:11 -0400

    clean up grade js

--- frontend/src/grade.js
+++ frontend/src/grade.js
@@ -3,24 +3,14 @@ class Grade {
         this.score = score;
     }
 
-
-
-
-
 static showScores() {
-    // submit.addEventListener("click", (showScores))
     submit.onclick = function() {
         console.log(score);
         submit.style.display = 'none'
-        // endPage = document.getElementById('quiz');
         quizGrid.innerHTML = "Your score is" + " " + score + "!"
         restart.style.display = 'block'
-    }
-
-
-    
+    }   
 }
 
-    
 }
 
